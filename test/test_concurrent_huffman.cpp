bc2c9e291cfba6e7bf2af1a083e058d6786e4418
jmsadair <jmsadair@gmail.com>
2022-08-11 14:49:31 -0400

    Added additional test

--- test/test_concurrent_huffman.cpp
+++ test/test_concurrent_huffman.cpp
@@ -115,3 +115,31 @@ TEST(Huffman, EncodingAndDecodingTest4)
     std::filesystem::remove("test4_encoded.txt");
     std::filesystem::remove("test4_decoded.txt");
 }
+
+// Tests encoding / decoding a file that contains a single character.
+TEST(Huffman, EncodingAndDecodingTest5)
+{
+    // Read the file to compressFile into memory.
+    std::string file_to_encode = "test5_input.txt";
+    std::ifstream file1(file_to_encode);
+    std::stringstream buffer1;
+    buffer1 << file1.rdbuf();
+    std::string expected_decoded_text = buffer1.str();
+
+    std::string encoded_file = "test5_encoded.txt";
+    std::string decoded_file = "test5_decoded.txt";
+    ConcurrentHuffman::compressFile(file_to_encode, encoded_file);
+    ConcurrentHuffman::decompressFile(encoded_file, decoded_file);
+
+    // Read the decoded file into memory.
+    std::ifstream file2(decoded_file);
+    std::stringstream buffer2;
+    buffer2 << file2.rdbuf();
+    std::string actual_decoded_text = buffer2.str();
+
+    ASSERT_EQ(expected_decoded_text, actual_decoded_text);
+
+    // Clean up the files created during the tests.
+    std::filesystem::remove("test5_encoded.txt");
+    std::filesystem::remove("test5_decoded.txt");
+}
